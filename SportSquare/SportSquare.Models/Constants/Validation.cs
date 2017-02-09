7613a9597da25a2c5543a70a7bded7631b615a55
Galvaneyes <m.v.manchev@gmail.com>
2017-02-09 02:56:24 +0200

    Added basic validation class

---
+++ SportSquare/SportSquare.Models/Constants/Validation.cs
@@ -0,0 +1,20 @@
+﻿namespace EF.Model.Constants
+{
+    public static class Validation
+    {
+        public const int NameMinLength = 2;
+        public const int NameMaxLength = 40;
+
+        public const int TitleMinLength = 2;
+        public const int TitleMaxLength = 50;
+
+        public const int AgeMinValue = 0;
+        public const int AgeMaxValue = 100;
+
+        public const int CityMinLength = 2;
+        public const int CityMaxLength = 40;
+
+        public const int PhoneMinLength = 0;
+        public const int PhoneMaxLength = 20;
+    }
+}
