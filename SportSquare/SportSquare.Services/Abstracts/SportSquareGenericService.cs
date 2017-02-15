0f0a97a2f92de531f7b92bb801c07be1a728de04
Galvaneyes <m.v.manchev@gmail.com>
2017-02-15 19:58:29 +0200

    Make it abstract class

---
+++ SportSquare/SportSquare.Services/Abstracts/SportSquareGenericService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using SportSquare.Data.Contracts;
+using SportSquare.Services.Contracts;
+
+namespace SportSquare.Services
+{
+    public abstract class SportSquareGenericService<T>:ISportSquareGenericSerivce<T> where T: class
+    {
+        private readonly IGenericRepository<T> repository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public SportSquareGenericService(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException("Repository cannot be null!");
+            }
+
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException("UnitOfWork cannot be null!");
+            }
+
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+    }
+}
