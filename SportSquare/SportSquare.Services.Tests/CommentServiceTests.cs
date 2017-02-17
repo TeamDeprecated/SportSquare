f5cbd510af2927fb3a16e91ae08c2e1401b4b2fd
Galvaneyes <m.v.manchev@gmail.com>
2017-02-17 15:44:21 +0200

    Rearrange

---
+++ SportSquare/SportSquare.Services.Tests/CommentServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+using SportSquare.Models;
+using SportSquare.Data.Contracts;
+using SportSquare.Services.Contracts;
+
+namespace SportSquare.Services.Tests
+{
+    [TestFixture]
+    public class CommentServiceTests
+    {
+        [Test]
+        public void ConstructorShouldThrowException_WhenRepositoryIsNull()
+        {
+            // Arrange
+            IGenericRepository<Comment> repository = null;
+            var unitOfWork = new Mock<IUnitOfWork>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new CommentService(repository, unitOfWork.Object));
+        }
+
+        [Test]
+        public void ConstructorShouldThrowException_WhenUnitOfWorkIsNull()
+        {
+            // Arrange
+            var repository = new Mock<IGenericRepository<Comment>>();
+            IUnitOfWork unitOfWork = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new CommentService(repository.Object, unitOfWork));
+        }
+
+        [Test]
+        public void ConstructorShouldCreateCommentService_WhenDependenciesAreCorrect()
+        {
+            // Arrange
+            var repository = new Mock<IGenericRepository<Comment>>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+
+            // Act
+            ICommentService commentService = new CommentService(repository.Object, unitOfWork.Object);
+
+            // Assert
+            Assert.IsNotNull(commentService);
+        }
+    }
+}
