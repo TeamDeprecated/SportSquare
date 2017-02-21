b4ab2ee258e0f77278f8120bad82078828cba337
Galvaneyes <m.v.manchev@gmail.com>
2017-02-21 20:16:28 +0200

    Changed logic

--- SportSquare/SportSquare.MVP/Views/AdminPanel/IEditVenuesView.cs
+++ SportSquare/SportSquare.MVP/Views/AdminPanel/IEditVenuesView.cs
@@ -1,4 +1,6 @@
-﻿using SportSquare.MVP.Models.Search;
+﻿using SportSquare.MVP.Models.AdminPanel;
+using SportSquare.MVP.Models.Search;
+using SportSquare.MVP.Models.VenueDetails;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,11 @@ using WebFormsMvp;
 
 namespace SportSquare.MVP.Views.AdminPanel
 {
-    public interface IEditVenuesView : IView<SearchViewModel>
+    public interface IEditVenuesView : IView<EditVenuesViewModel>
     {
         event EventHandler<SearchEventArgs> QueryEvent;
+
+        event EventHandler<BasicEventArgs> VenueDetailsId;
     }
 }
+    
