de6c7cd12aa6b9905505532420d59cef58af8223
Galvaneyes <m.v.manchev@gmail.com>
2017-02-21 20:15:43 +0200

    Added new event args

---
+++ SportSquare/SportSquare.MVP/Models/AdminPanel/BasicEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportSquare.MVP.Models.AdminPanel
+{
+    public class BasicEventArgs : EventArgs
+    {
+        public object Id { get; set; }
+
+        public BasicEventArgs(object id)
+        {
+            this.Id = id;
+        }
+    }
+}
