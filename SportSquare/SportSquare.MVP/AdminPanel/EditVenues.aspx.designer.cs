7574ea1e99cdd80a1a2ffdba9d2beff1dd27eea5
Galvaneyes <m.v.manchev@gmail.com>
2017-02-21 20:15:02 +0200

    Some ugly admin has been added

--- SportSquare/SportSquare.MVP/AdminPanel/EditVenues.aspx.designer.cs
+++ SportSquare/SportSquare.MVP/AdminPanel/EditVenues.aspx.designer.cs
@@ -56,5 +56,14 @@ namespace SportSquare.MVP.AdminPanel {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ListView VenueList;
+        
+        /// <summary>
+        /// VenueDetails control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FormView VenueDetails;
     }
 }
