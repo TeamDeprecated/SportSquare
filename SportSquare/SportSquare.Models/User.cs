ece223a147bf34a6298db102d1f1201754bade45
Galvaneyes <m.v.manchev@gmail.com>
2017-02-22 03:03:41 +0200

    Refactor

--- SportSquare/SportSquare.Models/User.cs
+++ SportSquare/SportSquare.Models/User.cs
@@ -1,36 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 using SportSquare.Enums;
 using SportSquare.Models.Contracts;
-using System;
 
 namespace SportSquare.Models
 {
     public class User : IDbModel
     {
-        // TODO: Must be added validations!
-
-        private ICollection<Rating> ratings;
         private ICollection<Comment> comments;
         private ICollection<UserFavoriteVenue> favoriteVenues;
+        private ICollection<Rating> ratings;
         private ICollection<UserWishVenue> wishVenues;
 
         public User()
         {
-            this.ratings = new HashSet<Rating>();
             this.comments = new HashSet<Comment>();
             this.favoriteVenues = new HashSet<UserFavoriteVenue>();
+            this.ratings = new HashSet<Rating>();
             this.wishVenues = new HashSet<UserWishVenue>();
+            this.IsHidden = false;
         }
 
-        public User(Guid aspNetUserId, string email, string firstName, string lastName, GenderType gender, int age) : this()
+        public User(Guid aspNetUserId, string email, string firstName, string lastName, GenderType gender, int age)
+            : this()
         {
             this.Id = aspNetUserId;
             this.Username = email;
             this.Email = email;
             this.FirstName = firstName;
             this.LastName = lastName;
-            this.Gender = Gender;
+            this.Gender = gender;
             this.Age = age;
         }
 
@@ -38,44 +38,38 @@ namespace SportSquare.Models
 
         public string Username { get; set; }
 
-        //[MinLength(2)]
-        //[MaxLength(10)]
         public string FirstName { get; set; }
 
-        //[MinLength(2)]
-        //[MaxLength(10)]
         public string LastName { get; set; }
 
         public GenderType Gender { get; set; }
 
-       public int Age { get; set; }
+        public int Age { get; set; }
 
-        //[Required]
-        //[EmailAddress]
         public string Email { get; set; }
 
         public virtual ICollection<Comment> Comments
         {
-            get { return this.comments; }
-            set { this.comments = value; }
+            get { return comments; }
+            set { comments = value; }
         }
 
         public virtual ICollection<Rating> Ratings
         {
-            get { return this.ratings; }
-            set { this.ratings = value; }
+            get { return ratings; }
+            set { ratings = value; }
         }
 
         public virtual ICollection<UserFavoriteVenue> FavoriteVenues
         {
-            get { return this.favoriteVenues; }
-            set { this.favoriteVenues = value; }
+            get { return favoriteVenues; }
+            set { favoriteVenues = value; }
         }
 
         public virtual ICollection<UserWishVenue> WishVenues
         {
-            get { return this.wishVenues; }
-            set { this.wishVenues = value; }
+            get { return wishVenues; }
+            set { wishVenues = value; }
         }
 
         public bool IsHidden { get; set; }
