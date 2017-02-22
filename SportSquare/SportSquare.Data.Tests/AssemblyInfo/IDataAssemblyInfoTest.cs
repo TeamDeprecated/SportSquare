75b8e91878af4d2522729d1ec3e8b09ba8f4b88a
Galvaneyes <m.v.manchev@gmail.com>
2017-02-22 07:33:44 +0200

    Added IDataAssembly tests

---
+++ SportSquare/SportSquare.Data.Tests/AssemblyInfo/IDataAssemblyInfoTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+using NUnit.Framework;
+
+using SportSquare.Data.AssemblyInfo;
+
+namespace SportSquare.Data.Tests.AssemblyInfo
+{
+    [TestFixture]
+    public class IDataAssemblyInfoTest
+    {
+        [Test]
+        public void ModelsAssembly_ShouldContainsIModelAssemblyInfo()
+        {
+            var assembly = typeof(IDataAssemblyInfo);
+            var result = Assembly.GetAssembly(assembly);
+
+            Assert.That(result.FullName, Is.Not.Null.And.Contains("SportSquare.Data"));
+        }
+    }
+}
